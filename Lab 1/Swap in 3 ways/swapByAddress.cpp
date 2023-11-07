2b23ae8b295be1bff3579a796aca734a1269885c
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2023-11-07 20:48:21 +0200

    Added Lab 1 (swap)

---
+++ Lab 1/Swap in 3 ways/swapByAddress.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void swapByAddress(int &x, int &y);
+//
+//int main()
+//{
+//    int x = 10, y = 5;
+//
+//    swapByAddress(x, y);
+//
+//    cout<<"-- Swap By Address --\n";
+//    cout<<"X : "<<x;
+//    cout<<"\n";
+//    cout<<"Y : "<<y;
+//
+//    return 0;
+//}
+//
+//void swapByAddress(int &x, int &y)
+//{
+//    int temp = x;
+//    x = y;
+//    y = temp;
+//}
---
+++ Lab 1/Swap in 3 ways/swapByReference.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void swapByReference(int* x, int* y);
+//
+//int main()
+//{
+//    int x = 10, y = 5;
+//
+//    swapByReference(&x, &y);
+//
+//    cout<<"-- Swap By Reference --\n";
+//
+//    cout<<"X : "<<x;
+//    cout<<"\n";
+//    cout<<"Y : "<<y;
+//
+//    return 0;
+//}
+//
+//
+//void swapByReference(int* x, int* y)
+//{
+//    int temp = *x;
+//    *x = *y;
+//    *y = temp;
+//}
